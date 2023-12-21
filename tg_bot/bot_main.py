1fd34bd86ef4f0d79d6251cb2d677866520b35f3
KliminAndrey <andrey7klimin@yandex.ru>
2023-12-21 10:56:17 +0300

    tg_struct

---
+++ tg_bot/bot_main.py
@@ -0,0 +1,23 @@
+
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+import user_msg
+import bot_msg
+#from user_msg import cmd_start, read_stat_freq, cmd_get_list, cmd_get_report, read_to_do
+#from bot_msg import print_msg, ping, button_mark_as_done, button_defer, print_todo_of_list, button_mark_as_done, button_defer
+from datetime import datetime
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    bot = Bot(token="")
+    dp = Dispatcher()
+    dp.include_routers(user_msg.router, bot_msg.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
