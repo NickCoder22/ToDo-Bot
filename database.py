05eaa53d89981f59cd391179d09ded7505ca52d1
XXZXJFFM41GH77 <aaaakkdbja@gmail.com>
2023-12-21 15:26:15 +0300

    removing rust

---
+++ database.py
@@ -0,0 +1,61 @@
+from surrealdb import Surreal
+
+async def create(db, chat, problem, deadline):
+    res = await db.create(
+        "todo",
+        {
+            "chat": chat,
+            "problem": problem,
+            "deadline": str(deadline),
+        }
+    )
+    return res[0]["id"]
+
+def edit(db, id, new_problem, new_deadline):
+    return db.query("""
+    UPDATE $id MERGE {
+        problem: $new_problem,
+        deadline: $new_deadline,
+    };
+    """, {
+        'id': id,
+        'new_problem': new_problem,
+        'new_deadline': str(new_deadline),
+    })
+
+def remove(db, id):
+    return db.delete(id)
+
+def get_todo(db, id):
+    return db.select(id)
+
+def done(db, id):
+    return db.query("""
+    UPDATE $id MERGE {
+        done: true
+    };
+    """, {
+        'id': id,
+    })
+
+async def get_report(db, chat, first_deadline, second_deadline):
+    res = await db.query("""
+    SELECT id, problem, deadline, done FROM todo
+        WHERE chat = $chat
+        AND deadline >= $form
+        AND deadline <= $to
+    """, {
+        'chat': chat,
+        'from': str(first_deadline),
+        'to': str(second_deadline),
+    })
+    return res[0]['result']
+
+
+async def connect(address,user,passwd,ns,db):
+    database = Surreal(address)
+    await database.connect()
+    await database.signin({"user": user, "pass": passwd})
+    await database.use(ns, db)
+    return database
+
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import database
+from database import *
 import asyncio
+import datetime
 
 async def main():
     dbcfg = open("db.cfg")
     address, user, passwd, ns, db = dbcfg.readline().split()
-    db = await database.connect(address, user, passwd, ns, db)
-    print(db)
-
+    db = await connect(address, user, passwd, ns, db)
 
 if __name__ == "__main__":
     asyncio.run(main())
